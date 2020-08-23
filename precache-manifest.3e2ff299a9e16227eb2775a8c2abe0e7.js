601c55468912fdf09d4f7e7138e9298d94afedec
Keerthilingu <keerthilingu@gmail.com>
2020-08-23 22:36:38 +0530

    Updates

---
+++ precache-manifest.3e2ff299a9e16227eb2775a8c2abe0e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b95c184362d2becb163e3feba7c2c5e5",
+    "url": "/react-slideshow-app/index.html"
+  },
+  {
+    "revision": "4d09c63cb739e9b46592",
+    "url": "/react-slideshow-app/static/css/main.372c6bbc.chunk.css"
+  },
+  {
+    "revision": "0f22a4d9643714b26e98",
+    "url": "/react-slideshow-app/static/js/2.07a9a178.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-slideshow-app/static/js/2.07a9a178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d09c63cb739e9b46592",
+    "url": "/react-slideshow-app/static/js/main.ff262baf.chunk.js"
+  },
+  {
+    "revision": "77a7a3c1de2fdc11cc97",
+    "url": "/react-slideshow-app/static/js/runtime-main.e0fd80a1.js"
+  }
+]);
